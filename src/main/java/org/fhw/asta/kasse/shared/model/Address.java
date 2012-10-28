1a4b1e81f338a72a740a56f2af9e9ca16a8720f6
clericc <julian.wefers@gmail.com>
2012-10-28 10:18:55 +0100

    Javadoc comments for model added

--- src/main/java/org/fhw/asta/kasse/shared/model/Address.java
+++ src/main/java/org/fhw/asta/kasse/shared/model/Address.java
@@ -2,12 +2,20 @@ package org.fhw.asta.kasse.shared.model;
 
 import java.io.Serializable;
 
+/**
+ * Container to represent a specific address stored in DB. An address always
+ * belongs to a single Person.
+ * 
+ * @author Julian Wefers
+ */
 public class Address implements Serializable
 {
   private static final long serialVersionUID = 1L;
 
+  /** Ldap name of the address owner */
   private String ownerLdapName;
 
+  /** Revision number of the address owner */
   private int ownerRevision;
 
   private String street;
@@ -18,6 +26,17 @@ public class Address implements Serializable
 
   private String streetnumber;
 
+  /**
+   * Creates an object representation for a specific address belonging to a
+   * specific person.
+   * 
+   * @param ownerLdapName
+   * @param ownerRevision
+   * @param street
+   * @param zipcode
+   * @param town
+   * @param streetnumber
+   */
   public Address(final String ownerLdapName, final int ownerRevision,
       final String street, final String zipcode, final String town,
       final String streetnumber)

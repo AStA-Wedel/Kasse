ef131326bf27ce7c083503134006ebe099b5ebd8
alex_bie <alex.biehl@gmail.com>
2012-10-28 15:54:38 +0100

    Validation for poor men

---
+++ src/main/java/org/fhw/asta/kasse/client/common/Validator.java
@@ -0,0 +1,53 @@
+package org.fhw.asta.kasse.client.common;
+
+import java.util.Iterator;
+import java.util.List;
+
+import com.google.common.base.Predicate;
+
+public class Validator<T> {
+
+	private List<Predicate<T>> predicates;
+	
+	static public <E> Validator<E> start() {
+		return new Validator<E>();
+	}
+	
+	public Validator<T> add(Predicate<T> predicate) {
+		this.predicates.add(predicate);
+		return this;
+	}
+	
+	public boolean validate(T arg) {
+	
+		Iterator<Predicate<T>> predicateIterator = predicates.iterator();
+
+		boolean ret = true;
+		
+		while (ret && predicateIterator.hasNext()) {
+			ret &= predicateIterator.next().apply(arg);
+		}
+		
+		return ret;
+	}
+
+	/*
+
+		boolean valid = Validator.start().add(new Predicate<Article> { 
+			
+			public boolean apply(Article a) {
+				return a.getId > 0;
+			}
+		
+		}).add(new Predicate<Article> {
+			
+			public boolean apply(Article a) {
+				return !Strings.isNullOrEmpty(a.getName());
+			}
+			
+		}).validate();
+		
+	}*/
+	
+	
+}

8b2edb2798f3b101901ae1e8f5ace04bf28afba2
inf9144 <alexander.buelow@gmail.com>
2013-03-23 16:01:46 +0100

    UserListActivity

--- src/main/java/org/fhw/asta/kasse/client/inject/module/ActivityModule.java
+++ src/main/java/org/fhw/asta/kasse/client/inject/module/ActivityModule.java
@@ -4,10 +4,12 @@ import org.fhw.asta.kasse.client.activity.ArticleListActivity;
 import org.fhw.asta.kasse.client.activity.BackofficeActivity;
 import org.fhw.asta.kasse.client.activity.LoginActivity;
 import org.fhw.asta.kasse.client.activity.PrintCustomsActivity;
+import org.fhw.asta.kasse.client.activity.UserListActivity;
 import org.fhw.asta.kasse.client.inject.module.factory.ArticleListActivityFactory;
 import org.fhw.asta.kasse.client.inject.module.factory.BackofficeActivityFactory;
 import org.fhw.asta.kasse.client.inject.module.factory.LoginActivityFactory;
 import org.fhw.asta.kasse.client.inject.module.factory.PrintCustomsActivityFactory;
+import org.fhw.asta.kasse.client.inject.module.factory.UserListActivityFactory;
 
 import com.google.gwt.activity.shared.Activity;
 import com.google.gwt.inject.client.AbstractGinModule;
@@ -21,6 +23,7 @@ public class ActivityModule extends AbstractGinModule {
 		install(new GinFactoryModuleBuilder().implement(Activity.class, ArticleListActivity.class).build(ArticleListActivityFactory.class));
 		install(new GinFactoryModuleBuilder().implement(Activity.class, BackofficeActivity.class).build(BackofficeActivityFactory.class));
 		install(new GinFactoryModuleBuilder().implement(Activity.class, PrintCustomsActivity.class).build(PrintCustomsActivityFactory.class));
+		install(new GinFactoryModuleBuilder().implement(Activity.class, UserListActivity.class).build(UserListActivityFactory.class));
 	}
 
 }

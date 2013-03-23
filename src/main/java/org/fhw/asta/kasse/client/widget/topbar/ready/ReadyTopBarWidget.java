b09695c140b7c654c0b3b0d8a9d1401f847ccd7d
alexbiehl <alex.biehl@gmail.com>
2013-03-23 18:02:08 +0100

    - inject DataSource in GenericDao

--- src/main/java/org/fhw/asta/kasse/client/widget/topbar/ready/ReadyTopBarWidget.java
+++ src/main/java/org/fhw/asta/kasse/client/widget/topbar/ready/ReadyTopBarWidget.java
@@ -2,9 +2,8 @@ package org.fhw.asta.kasse.client.widget.topbar.ready;
 
 import org.fhw.asta.kasse.client.widget.topbar.TopbarWidget;
 import org.fhw.asta.kasse.client.widget.topbar.modules.navbar.NavBarWidget;
-import org.fhw.asta.kasse.client.widget.topbar.modules.quickbox.QuickBoxWidget;
 import org.fhw.asta.kasse.client.widget.topbar.modules.searchbox.SearchBoxWidget;
 
-public interface ReadyTopBarWidget extends TopbarWidget, NavBarWidget, QuickBoxWidget, SearchBoxWidget {
+public interface ReadyTopBarWidget extends TopbarWidget, NavBarWidget, SearchBoxWidget {
 
 }

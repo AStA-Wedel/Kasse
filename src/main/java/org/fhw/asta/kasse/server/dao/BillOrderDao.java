be91d4b9b326373030146f5eda38644c0dff000e
Julian Wefers <julian.wefers@gmail.com>
2013-03-23 19:32:40 +0100

    db change, added stored procedure for inserting billOrder, now usabe from BillOrderDao

--- src/main/java/org/fhw/asta/kasse/server/dao/BillOrderDao.java
+++ src/main/java/org/fhw/asta/kasse/server/dao/BillOrderDao.java
@@ -2,15 +2,18 @@ package org.fhw.asta.kasse.server.dao;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Types;
 import java.util.List;
 
 import org.fhw.asta.kasse.shared.basket.BasketItem;
 import org.fhw.asta.kasse.shared.common.EuroAmount;
 import org.fhw.asta.kasse.shared.model.BillOrder;
 import org.fhw.asta.kasse.shared.model.OrderState;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.jdbc.core.SqlOutParameter;
+import org.springframework.jdbc.core.SqlParameter;
+import org.springframework.jdbc.object.StoredProcedure;
 
 import com.google.common.base.Optional;
 import com.google.inject.Singleton;
@@ -20,6 +23,8 @@ public class BillOrderDao extends GenericDao {
 
   private final BillOrderRowMapper billOrderMapper = new BillOrderRowMapper();
   private final BasketItemMapper basketItemMapper = new BasketItemMapper();
+  private final CreateBillOrderProcedure createBillOrderProcedure = new CreateBillOrderProcedure(this.template);
+  // private final Logger logger = Logger.getLogger(BillOrderDao.class);
 
   private class BillOrderRowMapper implements RowMapper<BillOrder> {
     @Override
@@ -29,13 +34,30 @@ public class BillOrderDao extends GenericDao {
           arg0.getInt(10));
     }
   }
-
   private class BasketItemMapper implements RowMapper<BasketItem> {
     @Override
     public BasketItem mapRow(ResultSet arg0, int arg1) throws SQLException {
       // TODO Auto-generated method stub
       return new BasketItem(arg0.getString(1), new EuroAmount(arg0.getInt(2)), arg0.getInt(3), arg0.getInt(4),
           arg0.getInt(5));
+    }
+  }
+  private class CreateBillOrderProcedure extends StoredProcedure {
+    public CreateBillOrderProcedure(JdbcTemplate template) {
+      super(template, "CREATE_BILL_ORDER");
+      this.setFunction(false);
+      this.declareParameter(new SqlParameter("i_receipient_ldap_name", Types.VARCHAR));
+      this.declareParameter(new SqlParameter("i_issuer_ldap_name", Types.VARCHAR));
+      this.declareParameter(new SqlParameter("i_paid_cash", Types.BOOLEAN));
+      this.declareParameter(new SqlParameter("i_discount", Types.TINYINT));
+      this.declareParameter(new SqlParameter("i_order_state", Types.CHAR));
+      this.declareParameter(new SqlOutParameter("o_bill_number", Types.INTEGER));
+      this.compile();
+    }
+    public int createBill(String receipientLdapName, String issuerLdapName, boolean paidCash, int discount,
+        char orderState) {
+      return (Integer) this.execute(receipientLdapName, issuerLdapName, paidCash, discount, orderState).get(
+          "o_bill_number");
     }
   }
 
@@ -54,20 +76,15 @@ public class BillOrderDao extends GenericDao {
         + "bca INNER JOIN article a WHERE bo.bill_id = ?", new Object[]{id}, this.basketItemMapper);
   }
 
-  public void saveBillOrder(List<BasketItem> items, int discount, String recipientLdapName, String isserLdapName) {
-    final KeyHolder keyholder = new GeneratedKeyHolder();
-    final String insertBillOrder = "INSERT INTO bill_order"
-        + "(receipient_revision, receipient_ldap_name, issuer_revision, issuer_ldap_name, paid_cash, state, discount) "
-        + "VALUES(" + "SELECT MAX(revision) FROM person WHERE ldap_name = RECEIPIENT_NAME," + "RECEIPIENT_NAME,"
-        + "SELECT MAX(revision) FROM person WHERE ldap_name = ISSUER_NAME," + "ISSUER_NAME," + "PAID_CASH," + "STATE,"
-        + "DISCOUNT);";
-    this.template.update(insertBillOrder, new Object[]{}, keyholder);
+  public void saveBillOrder(List<BasketItem> items, final int discount, final String receipientLdapName,
+      final String issuerLdapName, final char orderState) {
+    final int billNumber = this.createBillOrderProcedure.createBill(receipientLdapName, issuerLdapName, true, discount,
+        orderState);
 
-    for (final BasketItem bi : items) {
-      new StringBuilder().append("INSERT INTO BILL_CONTAINS_ARTICLE VALUES(" + "SELECT MAX(bill_id) FROM bill_order,"
-          + "ARTICLE_ID," + "SELECT MAX(article_revision) FROM article WHERE article_id = ARTICLE_ID," + "COUNT,"
-          + "FALSE," + "DISCOUNT);");
+    for (final BasketItem bo : items) {
+      this.template.update("INSERT INTO bill_contains_article " + "VALUES(" + "?," + "?,"
+          + "(SELECT MAX(article_revision) FROM article WHERE article_id = ?)," + "?," + "FALSE," + "?);",
+          new Object[]{billNumber, bo.getArticleId(), bo.getArticleId(), bo.getAmount(), bo.getDiscount()});
     }
-
   }
 }

2bfbe39d0de8e3e3643a3c1d4b9aa746e2f0995a
Julian Wefers <julian.wefers@gmail.com>
2013-03-24 10:23:27 +0100

    reformat

--- src/main/java/org/fhw/asta/kasse/shared/common/Triple.java
+++ src/main/java/org/fhw/asta/kasse/shared/common/Triple.java
@@ -3,38 +3,32 @@ package org.fhw.asta.kasse.shared.common;
 /*
  * 
  */
-public class Triple<D, E, F>
-{
+public class Triple<D, E, F> {
+	private D fst;
+	private E snd;
+	private F trd;
 
-  private final D fst;
-  private final E snd;
-  private final F trd;
+	public static <G, H, I> Triple<G, H, I> mk(final G fst, final H snd,
+			final I trd) {
+		return new Triple<G, H, I>(fst, snd, trd);
+	}
 
-  public static <G, H, I> Triple<G, H, I> mk(final G fst, final H snd, final I trd)
-  {
-    return new Triple<G, H, I>(fst, snd, trd);
-  }
+	private Triple(final D fst, final E snd, final F trd) {
+		this.fst = fst;
+		this.snd = snd;
+		this.trd = trd;
+	}
 
-  private Triple(final D fst, final E snd, final F trd)
-  {
-    this.fst = fst;
-    this.snd = snd;
-    this.trd = trd;
-  }
+	public D fst() {
+		return this.fst;
+	}
 
-  public D fst()
-  {
-    return this.fst;
-  }
+	public E snd() {
+		return this.snd;
+	}
 
-  public E snd()
-  {
-    return this.snd;
-  }
-
-  public F trd()
-  {
-    return this.trd;
-  }
+	public F trd() {
+		return this.trd;
+	}
 
 }

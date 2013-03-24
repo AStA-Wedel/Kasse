59af78a0ae665a8fd5557d610237684da3fc260f
Julian Wefers <julian.wefers@gmail.com>
2013-03-24 10:00:30 +0100

    added some senseless comment

--- src/main/java/org/fhw/asta/kasse/shared/common/Triple.java
+++ src/main/java/org/fhw/asta/kasse/shared/common/Triple.java
@@ -1,29 +1,39 @@
 package org.fhw.asta.kasse.shared.common;
-public class Triple<D, E, F> {
+
+/*
+ * 
+ */
+public class Triple<D, E, F>
+{
 
   private final D fst;
   private final E snd;
   private final F trd;
 
-  public static <G, H, I> Triple<G, H, I> mk(G fst, H snd, I trd) {
+  public static <G, H, I> Triple<G, H, I> mk(final G fst, final H snd, final I trd)
+  {
     return new Triple<G, H, I>(fst, snd, trd);
   }
 
-  private Triple(D fst, E snd, F trd) {
+  private Triple(final D fst, final E snd, final F trd)
+  {
     this.fst = fst;
     this.snd = snd;
     this.trd = trd;
   }
 
-  public D fst() {
+  public D fst()
+  {
     return this.fst;
   }
 
-  public E snd() {
+  public E snd()
+  {
     return this.snd;
   }
 
-  public F trd() {
+  public F trd()
+  {
     return this.trd;
   }
 
